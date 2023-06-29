5596741951c09118fbee65458b0b76bf52913198
= <=>
2023-06-29 15:14:33 +0800

    1

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -2,20 +2,21 @@
 # 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
 
 import os
-
-os.system("sudo pip3 install torch")
-os.system("sudo pip3 install torchvision")
-
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torchvision
-
 from torch.utils.data import DataLoader
-    
+
 
 class NeuralNetwork(nn.Module):
-    pass
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        # 添加你的神经网络结构
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.global_avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(64, 10)
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
@@ -27,9 +28,13 @@ def read_data():
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
 def main():
-    model = NeuralNetwork() # 若有参数则传入参数
+    model = NeuralNetwork()
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
     return model
-    
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ _03_Convolution_Neural_Network/train.py
@@ -0,0 +1,77 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torch.utils.data import DataLoader
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        # 定义你的模型结构
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.global_avgpool = nn.AdaptiveAvgPool2d(1)  # 添加全局平均池化层
+        self.fc = nn.Linear(64, 10)  # 最后一层分类器
+
+    def forward(self, x):
+        # 定义模型的前向传播
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.global_avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+
+
+def train(model, data_loader_train):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+    num_epochs = 10
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        for images, labels in data_loader_train:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)  # 修改此行
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss / len(data_loader_train)}")
+
+    # 保存模型参数
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    torch.save(model.state_dict(), parent_dir + '/pth/model.pth')
+
+
+
+def read_data():
+    # 数据预处理和数据加载器的定义
+    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True,
+                                                 transform=torchvision.transforms.ToTensor())
+    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False,
+                                               transform=torchvision.transforms.ToTensor())
+    data_loader_train = DataLoader(dataset=dataset_train, batch_size=64, shuffle=True)
+    data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
+    return dataset_train, dataset_val, data_loader_train, data_loader_val
+
+def main():
+    dataset_train, _, data_loader_train, _ = read_data()
+    model = NeuralNetwork()
+    train(model, data_loader_train)
+
+if __name__ == '__main__':
+    main()
