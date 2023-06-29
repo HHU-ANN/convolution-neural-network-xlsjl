d8fc95ffb77c725e654a5b8d0cafb0a01b48336a
= <=>
2023-06-29 16:13:25 +0800

    5

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,4 +1,4 @@
-# 在该文件NeuralNetwork类中定义你的模型 1
+# 在该文件NeuralNetwork类中定义你的模型 
 # 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
 
 import os
@@ -11,28 +11,49 @@ from torch.utils.data import DataLoader
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2)
+        # 定义你的模型结构
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.global_avgpool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Linear(64, 10)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.global_avgpool = nn.AdaptiveAvgPool2d(1)  # 添加全局平均池化层
+        self.fc = nn.Linear(256, 10)  # 最后一层分类器
 
     def forward(self, x):
         # 定义模型的前向传播
         x = self.conv1(x)
         x = self.relu(x)
         x = self.maxpool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.conv3(x)
+        x = self.relu(x)
         x = self.global_avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
 
 def read_data():
-    # 这里可自行修改数据预处理，batch大小也可自行调整
-    # 保持本地训练的数据读取和这里一致
-    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
-    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
-    data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
+    # 数据预处理和数据加载器的定义
+    transform_train = torchvision.transforms.Compose([
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.RandomCrop(32, padding=4),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+    transform_val = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True,
+                                                 transform=transform_train)
+    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False,
+                                               transform=transform_val)
+    data_loader_train = DataLoader(dataset=dataset_train, batch_size=64, shuffle=True)
     data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
@@ -40,7 +61,7 @@ def main():
     model = NeuralNetwork()
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
-    model.load_state_dict(torch.load(parent_dir + '/pth/model.pth', map_location='cpu'))
+    model.load_state_dict(torch.load(parent_dir + '/pth/model.pth', map_location="cpu"))
     return model
 
 if __name__ == '__main__':
--- _03_Convolution_Neural_Network/train.py
+++ _03_Convolution_Neural_Network/train.py
@@ -5,21 +5,28 @@ import torch.optim as optim
 import torchvision
 from torch.utils.data import DataLoader
 
-
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2)
+        # 定义你的模型结构
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.global_avgpool = nn.AdaptiveAvgPool2d(1)  # 添加全局平均池化层
-        self.fc = nn.Linear(64, 10)  # 最后一层分类器
+        self.fc = nn.Linear(256, 10)  # 最后一层分类器
 
     def forward(self, x):
         # 定义模型的前向传播
         x = self.conv1(x)
         x = self.relu(x)
         x = self.maxpool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.conv3(x)
+        x = self.relu(x)
         x = self.global_avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
@@ -28,7 +35,7 @@ class NeuralNetwork(nn.Module):
 
 def train(model, data_loader_train):
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     num_epochs = 10
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -43,7 +50,7 @@ def train(model, data_loader_train):
 
             optimizer.zero_grad()
             outputs = model(images)
-            loss = criterion(outputs, labels)  # 修改此行
+            loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
@@ -57,21 +64,34 @@ def train(model, data_loader_train):
     torch.save(model.state_dict(), parent_dir + '/pth/model.pth')
 
 
-
 def read_data():
     # 数据预处理和数据加载器的定义
+    transform_train = torchvision.transforms.Compose([
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.RandomCrop(32, padding=4),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+    transform_val = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
     dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True,
-                                                 transform=torchvision.transforms.ToTensor())
+                                                 transform=transform_train)
     dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False,
-                                               transform=torchvision.transforms.ToTensor())
+                                               transform=transform_val)
     data_loader_train = DataLoader(dataset=dataset_train, batch_size=64, shuffle=True)
     data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
+
 
 def main():
     dataset_train, _, data_loader_train, _ = read_data()
     model = NeuralNetwork()
     train(model, data_loader_train)
+
 
 if __name__ == '__main__':
     main()
